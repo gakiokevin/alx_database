16bf0c7ce6c95948f72026a2cb4ccb967458181a
gakiokevin <gakiokevin5@gmail.com>
2023-08-25 15:35:33 +0300

    Created a new table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- script that creates a table called first_table in the current database in your MySQL server.
+-- A query to create a new table
+CREATE TABLE IF NOT EXISTS first_table(
+   id INT,
+   name VARCHAR(256)
+);
+
+
