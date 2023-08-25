6feedbbb47ae848caaa9d36062ded52cf1247fa0
gakiokevin <gakiokevin5@gmail.com>
2023-08-25 16:01:02 +0300

    description about the table

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- A script  that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- A query to display information about your table.
+SHOW COLUMNS FROM first_table;
