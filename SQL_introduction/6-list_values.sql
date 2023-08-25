aa03fbd85c6f32bb81b86569e768b73a6f7df70b
gakiokevin <gakiokevin5@gmail.com>
2023-08-25 16:06:11 +0300

    listing all row in the table

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- A script script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- a query to list all row in the table
+SELECT * FROM first_table;
